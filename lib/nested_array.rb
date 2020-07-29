2feecdf7e9d35bbfe2694d6f05c20e0728f57c4e
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-29 20:34:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,8 +35,10 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
 
 
-  CONVENTIONAL_PRODUCE.sort {|a, b| a <=> b}
-  ORGANIC_PRODUCE.sort {|a, b| a <=> b}
+  CONVENTIONAL_PRODUCE.sort! {|a, b| a <=> b}
+  ORGANIC_PRODUCE.sort! {|a, b| a <=> b}
+
+  store_shevles_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
 
 def matrix_lookup(matrix, row, column)
